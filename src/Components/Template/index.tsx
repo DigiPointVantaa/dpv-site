1b4b5d0e78500fff38df5cbee8d34459f49dcd01
Gerpiili369 <elektromiikka@hotmail.com>
2021-04-28 10:15:16 +0300

    Added Template component

---
+++ src/Components/Template/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+class Template extends React.Component {
+    render() {
+        return (
+            <div className="Template">
+                HTML goes here
+            </div>
+        );
+    }
+}
+
+export default Template;
