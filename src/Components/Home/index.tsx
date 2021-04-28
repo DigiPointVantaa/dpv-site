0cc428ee9eac3bc244752694f85b55075b4f8ac2
Gerpiili369 <elektromiikka@hotmail.com>
2021-04-28 10:14:43 +0300

    Added Home component

---
+++ src/Components/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="Home">
+                da front peig
+            </div>
+        );
+    }
+}
+
+export default Home;
